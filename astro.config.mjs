7e7964f5970940403844a53e5f464f70f9a5fa19
lazymaan <mrlazylala@gmail.com>
2026-05-11 01:57:29 +0500

    Залиты первые две главы

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,30 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightGiscus from 'starlight-giscus';
+
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			plugins: [
+    			starlightGiscus({
+      				repo: 'lenivecmister-arch/AnotherWorld',
+      				repoId: 'R_kgDORB5HKg',
+      				category: 'Announcements',
+      				categoryId: 'DIC_kwDORB5HKs4C8vZw',
+    			}),
+  			],
+			title: 'Another World',
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Старые главы',
+					items: [{ autogenerate: { directory: 'Старые главы' } }],
 				},
 				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
+					label: 'Новые главы',
+					items: [{ autogenerate: { directory: 'Новые главы' } }],
 				},
 			],
 		}),
